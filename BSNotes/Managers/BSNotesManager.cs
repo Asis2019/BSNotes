356ab3d93cebe678a203ecd937a552d229a67057
Asis <47801367+Asis2019@users.noreply.github.com>
2023-02-02 16:10:25 +0200

    Initial Commit

---
+++ BSNotes/Managers/BSNotesManager.cs
@@ -0,0 +1,21 @@
+using System;
+using Zenject;
+
+namespace BSNotes.Managers
+{
+    internal class BSNotesManager : IInitializable, IDisposable
+    {
+        public BSNotesManager()
+        {
+        }
+
+
+        public void Initialize()
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
