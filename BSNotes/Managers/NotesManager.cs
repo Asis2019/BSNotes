04c414e726cf6a03d355385d96edb598288e5a38
Asis <47801367+Asis2019@users.noreply.github.com>
2023-02-06 07:01:13 +0200

    Fixed archive not working, fixed archiving file with same name

--- BSNotes/Managers/NotesManager.cs
+++ BSNotes/Managers/NotesManager.cs
@@ -19,10 +19,9 @@ internal class NotesManager
     public void ArchiveNote(Note note)
     {
         var directory = Path.Combine(UnityGame.UserDataPath, nameof(BSNotes));
-        var destinationFile = Path.Combine(Path.Combine(directory, "Archive"), note.GetFileName());
-        
-        Plugin.Log.Info($"File to move: {note.GetFileName()}, FilePath: {note.GetFilePath()}, FileDest: {destinationFile}");
-        
+        var archivedFileName =  $"{Path.GetRandomFileName()}_{note.GetFileName()}";
+        var destinationFile = Path.Combine(Path.Combine(directory, "Archive"), archivedFileName);
+
         File.Move(note.GetFilePath(), destinationFile);
     }
 
@@ -40,6 +39,10 @@ internal class NotesManager
             .ToArray();
 
         foreach (var file in files)
-            _notes.Add(new Note(file, file.OpenText().ReadToEnd()));
+        {
+            var streamReader = file.OpenText();
+            _notes.Add(new Note(file, streamReader.ReadToEnd()));
+            streamReader.Close();
+        }
     }
 }
